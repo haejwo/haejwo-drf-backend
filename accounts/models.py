e0e4ad10fcd2e1ea49813df12c8a3d730f66dedb
tnpfldyd <dkdldhs0322@gmail.com>
2023-04-04 02:53:50 +0900

    feat : 회원기능 추가(임시)
    - 회원 기능 임시로 추가
    - 평점로직은 다시 진행해야 할듯, 리뷰가 많아지면 속도저하 고려
    - 일반 회원 가입 뒤, 소셜 로그인 기능 추가 예정

---
+++ accounts/models.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.models import AbstractBaseUser
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from managers import CustomUserManager
+
+# Create your models here.
+ROLE_CHOICES = (
+        ('CU', '고객'),
+        ('CO', '업체'),
+        ('AD', '관리자'),
+    )
+
+CATEGORY_CHOICES = [
+    ('moving', '이사'),
+    ('flower', '꽃'),
+    ('other', '기타'),
+]
+
+class User(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=2, choices=ROLE_CHOICES, default='CU')
+
+    USERNAME_FIELD = 'email'
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+class Company(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    category = models.CharField(max_length=10, choices=CATEGORY_CHOICES, default='other')
+
+class Review(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    comment = models.TextField(max_length=500, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
